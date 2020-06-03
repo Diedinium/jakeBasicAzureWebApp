8e34df64682b9e0c85c3e39bc888a34bf495cf8e
Jake Hall <jakethomashall@gmail.com>
2020-06-03 22:36:14 +0100

    Add project files.

---
+++ jakeFirstAzureWebApp/Pages/DataTransfer.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace jakeFirstAzureWebApp.Pages
+{
+    public class DataTransferModel : PageModel
+    {
+        public List<TableItem> tableItems = new List<TableItem>();
+
+        public void OnGet()
+        {
+            List<TableItem> retreivedTableItems = HttpContext.Session.GetObject<List<TableItem>>("TableItemsStore");
+            if (retreivedTableItems.Count > 0)
+            {
+                tableItems = retreivedTableItems;
+            }
+        }
+    }
+}
---
+++ jakeFirstAzureWebApp/Pages/Index.cshtml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Session;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace jakeFirstAzureWebApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public List<TableItem> tableItems = new List<TableItem>();
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            tableItems = new TableItem().ReturnDemoList();
+            HttpContext.Session.SetObject("TableItemsStore", tableItems);
+        }
+    }
+
+    public class TableItem
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Count { get; set; }
+
+        public TableItem() { }
+
+        public TableItem(int ItemNumber)
+        {
+            Name = "Name " + ItemNumber;
+            Description = "Example Description " + ItemNumber;
+            Count = ItemNumber;
+        }
+
+        public List<TableItem> ReturnDemoList()
+        {
+            List<TableItem> tableItems = new List<TableItem>
+            {
+                new TableItem(1),
+                new TableItem(2),
+                new TableItem(3),
+                new TableItem(4)
+            };
+
+            return tableItems;
+        }
+    }
+
+    public static class SessionExtensions
+    {
+        public static void SetObject(this ISession session, string key, object value)
+        {
+            session.SetString(key, JsonConvert.SerializeObject(value));
+        }
+
+        public static T GetObject<T>(this ISession session, string key)
+        {
+            var value = session.GetString(key);
+            return value == null ? default(T) : JsonConvert.DeserializeObject<T>(value);
+        }
+    }
+}
